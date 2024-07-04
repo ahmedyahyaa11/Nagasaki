072d0325c8a7cf8e6b074285fc00b937dda4361b
Ahmed Yahya <164516512+ahmedyahyaa11@users.noreply.github.com>
2024-07-03 20:03:41 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) =>{
+panel.addEventListener("click", () =>{
+    removeAcctiveClasses();
+    panel.classList.add("active");
+});
+});
+function removeAcctiveClasses(){
+    panels.forEach((panel)=>{
+        panel.classList.remove("active");
+    })
+}
